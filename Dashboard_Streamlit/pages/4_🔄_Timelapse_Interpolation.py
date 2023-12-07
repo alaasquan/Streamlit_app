bfa6e5ffdcacc93189674b8af76b616a8396834e
alaasquan <106450043+alaasquan@users.noreply.github.com>
2023-12-07 13:09:21 +0100

    Update 4_🔄_Timelapse_Interpolation.py

--- Dashboard_Streamlit/pages/4_🔄_Timelapse_Interpolation.py
+++ Dashboard_Streamlit/pages/4_🔄_Timelapse_Interpolation.py
@@ -1,3 +1,4 @@
+
 from pathlib import Path
 import streamlit as st
 from io import BytesIO
@@ -6,27 +7,66 @@ import base64
 import os
 import tifffile as tiff
 import matplotlib.pyplot as plt
+import folium
+from folium.raster_layers import ImageOverlay
+from streamlit_folium import folium_static
+from branca.colormap import LinearColormap
+from PIL import Image
+import json
+import requests
+from streamlit_lottie import st_lottie
 
+def load_lottiefile(url: str):
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for bad responses
+    return response.json()
+
+
+def load_lottirurl(url: str):
+	r= requests.get(url)
+	if r.status_code != 200:
+		return None
+	return r.json
+
+loop= 'https://alaasquan.github.io/Streamlit_app/Dashboard_Streamlit/Animation_Lottiefiles/loop.json'
+lottie_loop=load_lottiefile(loop)
+
+
+col1, col2 = st.columns(2)
+
+# Text for column 1
+with col1:
+    st.title("Raster Timelapse and Comparison")
+# Text for column 2
+with col2:
+
+
+    st_lottie(lottie_loop,
+	   speed= 0.75,
+	   reverse=False,
+	   loop=True,
+	   quality='low',
+	   height='125px',
+	   width='125px',)
 # Specify the path to your GeoTIFF files
-base_path = Path(r"C:\Users\Alaa\Desktop\StreamlitHajji\Dashboard_Streamlit")
+base='https://alaasquan.github.io/Streamlit_app/Dashboard_Streamlit'
+base_path=str(base)
 attributes = ["Pression", "Temperature", "Vitesse-Vent"]
 
 # User input: Select attribute
 selected_attribute_index = st.slider("Select Attribute", min_value=0, max_value=len(attributes)-1, step=1)
 attribute_names = {0: "Pression", 1: "Temperature", 2: "Vitesse-Vent"}
 selected_attribute = attributes[selected_attribute_index]
-st.info(f"Vous avez choisi l'attribut : {attribute_names[selected_attribute_index]}")
+st.info(f"You have chosen the attribute : {attribute_names[selected_attribute_index]}")
 
 # Generate a list of file paths based on the selected attribute and fixed days
 days = [f"Jour{i}" for i in range(7)]
-selected_attribute_paths = [base_path / f"{day}-{selected_attribute}.tif" for day in days]
+selected_attribute_paths = [f"{base}/{day}-{selected_attribute}.tif" for day in days]
 
 # Display the generated paths
-st.write("Generated File Paths:")
-for i, path in enumerate(selected_attribute_paths):
-    
-    globals()[f"path{i}"] = path
 
+for i, path in enumerate(selected_attribute_paths):
+    globals()[f"path{i}"] = path
 
 # Replace tif_paths with the dynamically generated paths
 tif_paths = [
@@ -38,7 +78,7 @@ tif_paths = [
     path5,
     path6
 ]
-
+st.write(tif_paths)
 # Create a temporary directory to store individual frames
 temp_dir = "temp_frames"
 os.makedirs(temp_dir, exist_ok=True)
@@ -53,13 +93,17 @@ def update_and_save(frame, tif_path):
 
     # Plot the raster data
     plt.imshow(img, cmap='viridis', origin='upper', extent=(0, img.shape[1], 0, img.shape[0]))
-    plt.title(f"Raster Timelapse Frame {frame + 1} - {os.path.basename(tif_path)}")
+    
+    # Set the title to Jour{frame}
+    plt.title(f"Jour{frame}")
+    
     plt.axis('off')
 
     # Save the figure as an image in the temporary directory
     temp_path = os.path.join(temp_dir, f"frame_{frame}.png")
     plt.savefig(temp_path, format='png', bbox_inches="tight", pad_inches=0, transparent=True)
     plt.close(fig)
+
 
 # Create the animation and save frames
 for frame, tif_path in enumerate(tif_paths):
@@ -85,5 +129,62 @@ frames[0].save(
 # Encode the gif bytes to base64
 gif_base64 = base64.b64encode(gif_buffer.getvalue()).decode()
 
-# Display the gif in Streamlit using st.markdown and HTML
-st.markdown(f'<img src="data:image/gif;base64,{gif_base64}" alt="gif">', unsafe_allow_html=True)
+import rasterio
+
+# ... (previous code)
+
+# Create a folium map
+m = folium.Map(location=[28.7917, -7.0926], zoom_start=5)
+
+# Add the basemap or any other layers to your map
+
+# Extract bounds from GeoTIFF metadata
+with rasterio.open(tif_paths[0]) as src:
+    bounds = [[src.bounds.bottom, src.bounds.left], [src.bounds.top, src.bounds.right]]
+
+# Add the GIF layer to the map
+gif_layer = ImageOverlay(
+    'data:image/gif;base64,' + gif_base64,
+    bounds=bounds,
+    opacity=0.7,
+    name='GIF Layer'
+).add_to(m)
+
+
+# Pre-define three color palettes
+color_palettes = {
+    "Pression": ['#d7191c', '#fdae61', '#ffffbf', '#abdda4', '#2b83ba'],
+    "Temperature": ['#440154', '#414287', '#27808e', '#46c06f', '#fde725'],
+    "Vitesse-Vent": ['#0b0405', '#3e356b', '#3670a0', '#40b7ad', '#def5e5'],
+}
+
+# Define vmin and vmax for each attribute
+attribute_limits = {
+    "Pression": {"vmin": 0, "vmax": 100},
+    "Temperature": {"vmin": 0, "vmax": 50},
+    "Vitesse-Vent": {"vmin": 0, "vmax": 20},
+}
+
+min_value = attribute_limits[selected_attribute]["vmin"]
+max_value = attribute_limits[selected_attribute]["vmax"]
+
+
+# Choose the color palette based on the selected attribute
+selected_palette = color_palettes[selected_attribute]
+
+# Create a linear color map based on the selected palette
+color_map = LinearColormap(selected_palette, vmin=min_value, vmax=max_value)
+
+if selected_attribute == "Pression":
+    color_map.caption = f"{selected_attribute} (hPa)"
+elif selected_attribute == "Temperature":
+    color_map.caption = f"{selected_attribute} (°C)"
+elif selected_attribute == "Vitesse-Vent":
+    color_map.caption = f"{selected_attribute} (Km/h)"
+
+
+
+# Add the colormap to the map
+m.add_child(color_map)
+
+folium_static(m)
